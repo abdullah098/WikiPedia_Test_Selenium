6fc3ece5551dfec2bbbaa4796cb23177ac023135
Abdullah Iqbal <ai3449713@gmail.com>
2021-08-09 16:52:57 +0500

    WikiNews Scroll Updated

--- src/test/java/WikiNews.java
+++ src/test/java/WikiNews.java
@@ -1,8 +1,11 @@
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
 
 public class WikiNews {
 
@@ -17,10 +20,18 @@ public class WikiNews {
         browser.driver.get("https://www.wikipedia.org/");
 
         //scroll
-        WebElement element = browser.driver.findElement(new By.ByXPath("//*[@id=\"www-wikipedia-org\"]/div[7]/div[3]/div[5]/a"));
-        Actions actions = new Actions(browser.driver);
-        actions.moveToElement(element);
-        actions.perform();
+        WebElement element = browser.driver.findElement(By.xpath("//*[@id=\"www-wikipedia-org\"]/div[7]/div[3]/div[5]/a"));
+        ((JavascriptExecutor) browser.driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+//        WebElement element = browser.driver.findElement(new By.ByXPath("//*[@id=\"www-wikipedia-org\"]/div[7]/div[3]/div[5]/a"));
+//        Actions actions = new Actions(browser.driver);
+//        actions.moveToElement(element);
+//        actions.perform();
 
 
         //click wikinews
